a97d0ed197e72cef87b9f063d35d217ef9d33c9e
KZ <github@kez.io>
2020-12-14 04:24:45 -0500

    Attempt to fix this.

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -2,10 +2,17 @@ import App
 import Vapor
 
 @discardableResult
-func shell(currentDirectoryURL: URL, executableURL: URL, args: String...) -> Int32 {
+func shell(currentDirectoryURL: URL, args: String...) -> Int32 {
     let task = Process()
     task.currentDirectoryURL = currentDirectoryURL
-    task.executableURL = executableURL
+    task.executableURL = URL(fileURLWithPath: "/usr/bin/env")
+
+    var env = task.environment ?? [:]
+    let homeDirectoryURL = FileManager.default.homeDirectoryForCurrentUser
+    let npmURL = homeDirectoryURL.appendingPathComponent(".nvm/versions/node/v15.4.0/bin")
+    env["PATH"] = "/usr/local/bin:\(npmURL.path)"
+    task.environment = env
+
     task.arguments = args
     task.launch()
     task.waitUntilExit()
@@ -17,8 +24,8 @@ try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
 
 let directoryURL = URL(fileURLWithPath: app.directory.workingDirectory)
-shell(currentDirectoryURL: directoryURL, executableURL: URL(fileURLWithPath: "~/.nvm/versions/node/v15.4.0/bin/npm"), args: "install")
-shell(currentDirectoryURL: directoryURL, executableURL: URL(fileURLWithPath: "~/.nvm/versions/node/v15.4.0/bin/npm"), args: "run", "build")
+shell(currentDirectoryURL: directoryURL, args: "npm", "install")
+shell(currentDirectoryURL: directoryURL, args: "npm", "run", "build")
 
 defer { app.shutdown() }
 try configure(app)
